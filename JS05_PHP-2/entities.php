11b051e9e8a5744c51f2345b7cf1dcdbfc1be8b7
m-fatahillah <fatahcrown789@gmail.com>
2025-10-01 08:16:53 +0700

    18 js5

---
+++ JS05_PHP-2/entities.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <title>Entities HTML</title>
+</head>
+<body>
+    <p>It&#39; time to read a HTML5 book.</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama Entities : 
+        sebuah nama entitas mudah diingat. Kerugian dari menggunakan nama Entities : Browser mungkin tidak mendukung semua nama entitas, tetapi dukungan untuk nomor lebih baik.</p>
+    <p>&#169; 2023 jti.com</p>
+</body>
+</html> 
